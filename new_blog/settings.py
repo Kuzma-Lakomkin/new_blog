66c1a030e033c3fe2e37d77249d7365abf6562f2
Igor Vasin <igor.vasin.1993@mail.ru>
2023-04-24 15:18:46 +0300

    Ссылка на выход; Добавление нового приложения 'accounts'; Регистрация пользователя.

--- new_blog/settings.py
+++ new_blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
